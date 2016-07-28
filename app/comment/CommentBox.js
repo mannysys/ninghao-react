cc459f797124fccdb7caac61afee624f1105a76c
zhoujialin <724475200@qq.com>
2016-07-28 16:21:32 +0800

    git add

---
+++ app/comment/CommentBox.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+import CommentList from './CommentList';
+import CommentForm from './CommentForm';
+
+class CommentBox extends React.Component{
+
+    render(){
+        return(
+            <div className="ui comments">
+                <h1>评论</h1>
+                <div className="ui divider"></div>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+
+    }
+
+}
+
+export { CommentBox as default };
