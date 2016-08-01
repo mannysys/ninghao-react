1c874ef6141a1d63656cd21ecd260cb2339ac68e
zhoujialin <724475200@qq.com>
2016-08-01 14:52:44 +0800

    git add

--- app/main.js
+++ app/main.js
@@ -3,16 +3,31 @@
 import 'semantic-ui/semantic.min.css!';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Link, IndexRoute } from 'react-router';
-
+import { Router, Route, Link, IndexRoute, Redirect } from 'react-router';
+/**
+ * react路由
+ */
 class App extends React.Component{
+    //加载组件后
+    componentDidMount(){
+        console.log('App did mount');
+    }
+    //离开组件后
+    componentWillReceiveProps(){
+        console.log('App will receive props');
+    }
+    //更新了组件
+    componentDidUpdate(){
+        console.log('App did update');
+    }
+
     render(){
         return (
             <div>
                 <div className="ui secondary pointing menu">
-                    { /* 跳转路径 */}
+                    { /* 跳转路径   orderBy是查询符(http://localhost:3000/#/tv?orderBy=date&_k=zp7fmc) */}
                     <Link to="/" className="item">首页</Link>
-                    <Link to="/tv" className="item">电视</Link>
+                    <Link to="/tv" className="item" query={{orderBy: 'date'}}>电视</Link>
                 </div>
                 {this.props.children}
             </div>
@@ -21,6 +36,16 @@ class App extends React.Component{
 }
 
 class TV extends React.Component{
+    constructor(props){
+        super(props);
+
+        //获取到路由的查询符
+        let { query } = this.props.location;
+        console.log(query);
+    }
+    componentDidMount(){
+        console.log('TV did mount');
+    }
     render(){
         return (
             <div>
@@ -44,6 +69,13 @@ class Show extends React.Component{
 }
 
 class Home extends React.Component{
+    componentDidMount(){
+        console.log('Home did mount');
+    }
+    //卸载了组件
+    componentWillUnmount(){
+        console.log('Home will unmount');
+    }
     render(){
         return (
             <div className="ui info message">首页内容</div>
@@ -57,10 +89,22 @@ class ShowIndex extends React.Component{
         );
     }
 }
+
+function handleEnter(){
+    console.log('进入');
+}
+function handleLeave(){
+    console.log('离开');
+}
+
 /**
  * path 是指定路径
  * component 是路径对应渲染的组件
  * IndexRoute 如果App组件里的children未定义的时候，就会显示默认的组件
+ * path="shows/:id" 如果在shows前面加上/ 就是使用绝对路径,可以不输入tv直接访问
+ * Redirect 重定向
+ * onEnter 进入这个路由的时候触发
+ * onLeave 离开这个路由的时候触发
  */
 ReactDOM.render((
     <Router>
@@ -68,7 +112,11 @@ ReactDOM.render((
             <IndexRoute component={Home} />
             <Route path="tv" component={TV}>
                 <IndexRoute component={ShowIndex} />
-                <Route path="shows/:id" component={Show}/>
+                <Route path="/shows/:id" component={Show}
+                    onEnter={handleEnter}
+                    onLeave={handleLeave}
+                />
+                <Redirect from="shows/:id" to="/shows/:id" />
             </Route>       
         </Route>
     </Router>
